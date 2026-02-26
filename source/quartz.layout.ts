40590666cbe4b1ba84ea682ffce5285f6dc5f11c
Steve Onorato <steveonoratojr@gmail.com>
2026-02-25 23:31:13 -0500

    vault backup: 2026-02-25 23:31:12

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -6,12 +6,7 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.Footer({
-  links: {
-    [`© ${new Date().getFullYear()} Stensin. Enutheta is an original universe and world setting.`]: "",
-    "Enutheta lore archives.": "https://lore.enutheta.com",
-  },
-})
+  footer: Component.Footer({ links: {} }),
 }
 
 // components for pages that display a single page (e.g. a single note)
