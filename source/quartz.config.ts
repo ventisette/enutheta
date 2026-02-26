7b06d619660f2f749fbebefaf2c3036be45b0711
Steve Onorato <steveonoratojr@gmail.com>
2026-02-25 22:31:40 -0500

    vault backup: 2026-02-25 22:31:40

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🌃 Enutheta Wiki",
+    pageTitle: "[icon.png] Enutheta Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
