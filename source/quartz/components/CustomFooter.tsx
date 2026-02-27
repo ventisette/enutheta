25e0eb3b8f9806840a2d65e4bf35c126d684eb8d
Steve Onorato <steveonoratojr@gmail.com>
2026-02-26 22:28:37 -0500

    vault backup: 2026-02-26 22:28:37

--- source/quartz/components/CustomFooter.tsx
+++ source/quartz/components/CustomFooter.tsx
@@ -6,11 +6,11 @@ export default (() => {
 
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>© {year} Stensin. Enutheta is an original universe concept and world setting.</p>
+        <p>© 2020 - {year} Stensin. Enutheta is an original universe concept and world setting.</p>
         <ul>
           <li>
             <a href="https://lore.enutheta.com" target="_blank" rel="noopener noreferrer">
-              Enutheta lore archives.
+              Visit the Enutheta lore archives
             </a>
           </li>
         </ul>
